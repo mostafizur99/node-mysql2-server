956d5f6b22f6e64febb8399b078893344aed4da1
mostafizur99 <coder.mr11@gmail.com>
2022-10-23 20:39:45 +0600

    feat: sql data insert added

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 require('dotenv').config()
 const sqlQueries = require('./sqlQuery')
 
-// fetch all data 
-sqlQueries.fetchAll()
-    .then(([allData, fieldData]) => {
-        console.log(allData);
-        console.log(fieldData);
+// =======fetch all data==========
+// sqlQueries.fetchAll()
+//     .then(([allData, fieldData]) => {
+//         console.log(allData);
+//         console.log(fieldData);
+//     })
+//     .catch(err => {
+//         console.log(err);
+//     });
+
+
+// =======insert data==========
+const insertData = {
+    name: 'John Young',
+    age: '28',
+    gender: 'Male',
+    salary: '3000'
+}
+sqlQueries.insert(insertData)
+    .then(() => {
+        console.log('Data Inserted Successfully');
     })
     .catch(err => {
-        console.log(err);
+        console.error('Data Insertion Failed', err);
     });
+
--- sqlQuery.js
+++ sqlQuery.js
@@ -3,6 +3,11 @@ const db = require('./dbConnect')
 const sqlQueries = {
     fetchAll: () => {
         return db.execute('SELECT * FROM employee_data')
+    },
+    insert: (insertData) => {
+        return db.execute(
+            'INSERT INTO employee_data (name, age, gender, salary) VALUES(?, ?, ?, ?)', [insertData.name, insertData.age, insertData.gender, insertData.salary]
+        );
     }
 
 }
